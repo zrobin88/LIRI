43cf7ed1d2e75498cdb452d719bb2d963c6b1551
Zachary Robin <zacharyrobin@zacharys-mbp-2.home>
2019-02-09 13:27:38 -0500

    final product

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # LIRI
+
+video demo: https://drive.google.com/file/d/1AKNpZmt0qy7tMXpemx9gGPOJpZksKLNb/view
+
+In order for this application to work, we need to install the following npm packages:
+
+- axios
+- moment.js
+- fs 
+- node-spotify-api
+
+
+to search for a song enter the command: node-liri.js spotify-this-song <song name>
+
+to search for a concert enter the command: node liri.js concert-this <artist name>
+
+to search for a movie enter the command: node liri.js movie-this <movie name>
--- liri.js
+++ liri.js
@@ -1,53 +1,131 @@
-// Includes the FS package for reading and writing packages
+// Load the fs package to read and write
 var fs = require("fs");
-// Running the readFile module that's inside of fs.
-// Stores the read information into the variable "data"
-fs.readFile("random.txt", "utf8", function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-});
-
-    require("dotenv").config();
-
-    var axios = require("axios");
-
-    var Spotify = require('node-spotify-api');
-
-    var keys = require("./keys.js");
-
-    var inquirer = require("inquirer");
 
 
+//requirements and variables 
+
+require("dotenv").config();
+
+var moment = require("moment"); 
+
+var axios = require("axios");
+
+var Spotify = require("node-spotify-api");
+
+var keys = require("./keys.js");
 
 
+var timeFormat = moment().format("ddd, MM/DD/YYYY");
 
+
+//vars for taking in commands 
+let command = process.argv[2];
+let userSong = process.argv[3];
+let userMovie = process.argv.slice(3).join(" ");
+let userConcert = process.argv.slice(3).join(" ");
+
+
+spotifySearch = ()=>{
     var spotify = new Spotify(keys.spotify);
-
-
-
-    let userSong = process.argv[2];
     spotify.search({ type: 'track', query: userSong }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
-        console.log(data);
+        //for (let i = 0; i < data.tracks.items.length; i++) {
+            console.log("Artist: " + data.tracks.items[0].artists[0].name)
+            console.log("Track: " + data.tracks.items[0].name); 
+            console.log("Preview URL: " + data.tracks.items[0].preview_url);
+            console.log("Album: "+ data.tracks.items[0].album.name);
+            
+           
+        //}
+           
     });
+}
 
-
-
-     //OMDB 
-     let userMovie = process.argv[2]; 
-     axios.get("http://www.omdbapi.com/?t="+userMovie+"&y=&plot=short&apikey=trilogy").then(
+//movie search function
+movieSearch = () => {
+    axios.get("http://www.omdbapi.com/?t=" + userMovie + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
-            // Then we print out the imdbRating
-            console.log(data);
+
+            console.log("Title: " + response.data.Title);
+            console.log("Year Released: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Country Produced: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+
         }
+
+    );
+}
+//concert search function
+concertSearch = () => {
+
+    axios.get("https://rest.bandsintown.com/artists/" + userConcert + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            //loop through response to get ALL upcoming concert info from bandisintown
+            for (let i = 0; i < response.data.length; i++) {
+                console.log("Venue: "+response.data[i].venue.name);
+                console.log("City: "+ response.data[i].venue.city);
+               // var convertedDate = moment(response.data.datetime, timeFormats); 
+                console.log("Date: " + response.data[i].datetime);
+                //console.log("Date: "+ convertedDate);           
+            }
+            
+        }
+
     );
 
+}
 
+
+//switch case for determing the proper API to call 
+
+switch (command) {
+    //Spotify
+    case 'spotify-this-song':
+        spotifySearch(); 
+        break;
+
+    //OMDB 
+    case 'movie-this':
+        movieSearch(userMovie);
+        //if user enters no title, it will default to "Mr. Nobody"
+        if (userMovie === "") {
+            axios.get("http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy").then(
+                function (response) {
+
+                    console.log(response.data);
+
+
+                }
+
+            );
+        }
+
+        break;
+
+        
+    //Band is in town
+    case 'concert-this':
+        concertSearch(userConcert);
+        break;
+        
+    case 'do-what-it-says':
+        fs.readFile("random.txt", "utf8", function(err, data) {
+            console.log(data);
+            if (err) {
+            return console.log(err);
+            }});
        
+        break;     
+        
+        
+    }    
+
+
 
 
 
